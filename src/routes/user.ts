51fe7594786658fead3b1cb13bf599c46f2a1d97
Bar Tzadok <bar4488@gmail.com>
2020-04-05 20:13:29 +0300

    refactor and delete files

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import UserController from "../controller/UserController";
-import { checkJwt } from "../middleware/checkJwt";
+import { validateToken } from "../middleware/checkJwt";
 
 const router = Router();
 
 //Get all users
-router.get("/", [checkJwt], UserController.listAll);
+router.get("/", [validateToken], UserController.listAll);
 
 // Get one user
 router.get(
     "/:id([0-9]+)",
-    [checkJwt],
+    [validateToken],
     UserController.getOneById
 );
 
@@ -21,14 +21,14 @@ router.post("/", UserController.newUser);
 //Edit one user
 router.patch(
     "/:id([0-9]+)",
-    [checkJwt],
+    [validateToken],
     UserController.editUser
 );
 
 //Delete one user
 router.delete(
     "/:id([0-9]+)",
-    [checkJwt],
+    [validateToken],
     UserController.deleteUser
 );
 
